2e5f7d55b572a618f7cd9988ebde7e72cae26ce7
kammitama5 <krystal.maughan@gmail.com>
2016-07-11 11:34:57 -0700

    Javascript Practice
    From Intro to Javascript GDI workshop part 1

---
+++ mycode.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+
+var potato = "hello ";
+var head = "Krystal"
+console.log(potato + head);
+
+function fullName(first, last)
+{
+	console.log('Hello ' + first + last + '.');
+
+}
+
+fullName('Waynie ', 'Robinson'); //outputs Hello Waynie Robinson
+fullName('Michelle ', "Watabe"); //outputs Hello Michelle Watabe
